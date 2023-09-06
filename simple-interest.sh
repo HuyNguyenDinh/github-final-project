6fa2a37b7d9fe83cb6ed5362c0695c402dee7ff5
Dinh Huy <huyn27316@gmail.com>
2023-09-06 08:53:18 +0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # huyn27316
+   # HuyNguyenDinh
    # Input:
    # p, principal amount
    # t, time period in years
